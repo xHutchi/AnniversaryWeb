427bdc48b0b4f428022ff023775584aeec4eec75
xHutchi <joehutchyt@gmail.com>
2023-09-21 19:07:23 +0100

    DuckEgg

--- script.js
+++ script.js
@@ -520,3 +520,306 @@ function removeAllChildren(parent) { //used when wiping a dynamic DOM element
 
 
 
+function DuckEgg () {
+    DuckEgg.prototype.name = "";
+    DuckEgg.prototype.isEgg = false;
+    DuckEgg.prototype.heart = 0;
+    DuckEgg.prototype.fedToday = false;
+
+
+}
+
+function checkFeed(DuckEgg) {
+    let today = new Date()
+    let todaystring = today.toDateString();
+    if (todaystring != DuckEgg.lastfed) {
+        DuckEgg.fedToday = false;
+
+        let lastupdated = new Date(DuckEgg.lastupdated);
+        let dayssince = Math.floor((today.getTime() - lastupdated.getTime()) / (1000 * 3600 * 24));
+        console.log(lastupdated.toDateString() + " " + dayssince +  " " + DuckEgg.heart);
+        if (dayssince >= 2) {
+            DuckEgg.heart -= 0.25;
+            DuckEgg.lastupdated = todaystring;
+        }
+    }
+    
+
+
+
+}
+
+function feedDuckEgg(DuckEgg) {
+    if (!DuckEgg.fedToday)
+    if (DuckEgg.isEgg) {
+        DuckEgg.heart += 0.25
+    } else {
+        DuckEgg.heart += 1/16;
+    }
+    DuckEgg.fedToday = true;
+    DuckEgg.lastfed = new Date().toDateString();
+    reRenderDuckEggs();
+
+
+}
+
+function CreateDuckEgg() {
+    let newEgg = new DuckEgg();
+    newEgg.heart = 0;
+    newEgg.isEgg = true;
+    newEgg.fedToday = false;
+    DuckEggs.push(newEgg);
+    reRenderDuckEggs();
+
+}
+
+function HatchDuckEgg(DuckEgg) {
+    DuckEgg.isEgg = false;
+    DuckEgg.heart = 0;
+    DuckEgg.fedToday = false;
+    reRenderDuckEggs();
+}
+
+function setDuckName(DuckEgg, name) {
+    DuckEgg.name = name.value;
+    reRenderDuckEggs();
+}
+
+let DuckEggs = []
+
+
+function reRenderDuckEggs() {
+    console.log(DuckEggs)
+    let duckArea = document.getElementById("duckarea");
+    removeAllChildren(duckArea);
+    if (DuckEggs.length <= 0) {
+        let stored = JSON.parse(localStorage.getItem("DuckEggs"))
+        if (stored != undefined && stored.length > 0) {
+            DuckEggs = stored;
+        } else {
+            let testDuck = new DuckEgg();
+            testDuck.name = ""
+            testDuck.heart = 0;
+            testDuck.isEgg = false;
+            testDuck.fedToday = false;
+            DuckEggs.push(testDuck); 
+        }
+    }
+
+    console.log(DuckEggs);
+
+    DuckEggs.forEach( (Ele)=> {
+        checkFeed(Ele);
+        if (!Ele.isEgg) {
+            let div = document.createElement("div");
+            div.className = "duckdiv flex flex-col w-fit gap-3 rounded-2xl p-4"
+            
+            let heartdiv = document.createElement("div");
+            heartdiv.className = "hearteggs flex justify-between"
+
+            if (Ele.heart > 1) {
+                Ele.heart = 1;
+            }
+
+            let heartsegments = Math.floor(Ele.heart * 16);
+
+            let fullhearts = Math.floor(heartsegments / 4);
+
+
+            for (let i = 0; i < fullhearts; i++) {
+                let heartele = document.createElement("img");
+                heartele.src = "Data/redheart.png"
+                heartdiv.appendChild(heartele)
+            }
+
+            let remaininghearts = heartsegments - (fullhearts * 4) ;
+            
+            let semiheartele = document.createElement("img");
+            switch((remaininghearts)) {
+                case 1:
+                    semiheartele.src="Data/redheart1.png" 
+                    break;
+                case 2:
+                    semiheartele.src="Data/redheart2.png" 
+                    break;
+                case 3:
+                    semiheartele.src="Data/redheart3.png" 
+                    break;
+            }
+
+            heartdiv.appendChild(semiheartele);
+
+            let greyhearts = Math.floor((16 - heartsegments) /4);
+
+            for (let i = 0; i < greyhearts; i++) {
+                let heartele = document.createElement("img");
+                heartele.src = "Data/greyheart.png"
+                heartdiv.appendChild(heartele)
+            }
+            
+            div.appendChild(heartdiv);
+
+            let duckimg = document.createElement("img");
+            duckimg.className = "duckimage w-fit m-auto drop-shadow-lg"
+            duckimg.src = "Data/duckplaceholder.png";
+
+            div.appendChild(duckimg);
+
+            if (Ele.name != "") {
+
+            let duckname = document.createElement("h2")
+            duckname.className = "duckname justmeagaindownhere text-white text-6xl m-auto";
+            duckname.innerText = Ele.name;
+
+            div.appendChild(duckname);
+
+            } else {
+                let namediv = document.createElement("div");
+                namediv.className = "ducknameenter w-full flex gap-2";
+
+                let input = document.createElement("input");
+                input.className = "w-full h-full p-2 text-2xl rounded-md";
+                input.placeholder = "Enter Duck Name"
+                input.type = "text"
+                namediv.appendChild(input);
+
+                let enterbtn = document.createElement("a");
+                enterbtn.className = "text-white my-auto hover:text-gray-500"
+                enterbtn.addEventListener("click", () => {
+                    setDuckName(Ele, input)
+
+                })
+
+                let entericon = document.createElement("i");
+                entericon.className = "fas fa-solid fa-right-to-bracket fa-2xl";
+                enterbtn.appendChild(entericon);
+
+                namediv.appendChild(enterbtn);
+
+                div.appendChild(namediv);
+
+
+            }
+
+            let duckbuttons = document.createElement("div");
+            duckbuttons.className = "duckbuttons flex justify-around gap-3"
+
+            let feedbutton = document.createElement("button");
+            feedbutton.className = "justmeagaindownhere  hover:bg-gray-500 bg-gray-300 rounded-2xl w-full p-3 text-4xl disabled:bg-gray-700 ";
+            
+            if (Ele.fedToday) {
+                feedbutton.innerText = "Fed Today"; 
+                feedbutton.disabled = true;
+            } else {
+            feedbutton.innerText = "Feed";
+            }
+
+
+            feedbutton.addEventListener("click", (a) => {
+                feedDuckEgg(Ele);
+            })
+
+            duckbuttons.appendChild(feedbutton);
+
+            let Eggbutton = document.createElement("button");
+            Eggbutton.className = "justmeagaindownhere  hover:bg-gray-500 bg-gray-300 rounded-2xl w-full p-3 text-4xl disabled:bg-gray-700 ";
+            Eggbutton.innerText = "Egg";
+            if (Ele.heart < 1) {
+                Eggbutton.disabled = true;
+            } 
+            Eggbutton.addEventListener("click", (a) => {
+                CreateDuckEgg();
+            })
+
+            duckbuttons.appendChild(Eggbutton);
+
+            div.appendChild(duckbuttons);
+
+            duckArea.appendChild(div);
+
+
+
+        }
+        else {
+            let div = document.createElement("div");
+            div.className = "duckdiv flex flex-col w-fit gap-3 rounded-2xl p-4"
+
+            let eggimg = document.createElement("img");
+            eggimg.className = "duckimage w-fit m-auto drop-shadow-lg"
+
+            if (Ele.heart > 1) {
+                Ele.heart = 1;
+            }
+
+            let eggstage = Math.floor(Ele.heart * 4);
+
+            
+
+            switch (eggstage) {
+                case 0:
+                    eggimg.src = "Data/egg1.png";
+                    break;
+                case 1:
+                    eggimg.src = "Data/egg2.png";
+                    break;
+                case 2:
+                    eggimg.src = "Data/egg3.png";
+                    break;
+                case 3:
+                    eggimg.src = "Data/egg4.png";
+                    break;
+                case 4:
+                    eggimg.src = "Data/egg4.png";
+                    break;
+                default: 
+                    eggimg.src = "Data/egg4.png"
+                    
+            }
+
+
+            div.appendChild(eggimg);
+
+            let duckbuttons = document.createElement("div");
+            duckbuttons.className = "duckbuttons flex justify-around gap-3"
+
+            let feedbutton = document.createElement("button");
+            feedbutton.className = "justmeagaindownhere  hover:bg-gray-500 bg-gray-300 rounded-2xl w-full p-3 text-4xl disabled:bg-gray-700 ";
+            if (Ele.fedToday) {
+                feedbutton.innerText = "Fed Today"; 
+                feedbutton.disabled = true;
+            } else {
+            feedbutton.innerText = "Feed";
+            }
+
+            feedbutton.addEventListener("click", (a) => {
+                console.log("yue")
+                feedDuckEgg(Ele);
+            })
+
+            duckbuttons.appendChild(feedbutton);
+
+            let Eggbutton = document.createElement("button");
+            Eggbutton.className = "justmeagaindownhere  hover:bg-gray-500 bg-gray-300 rounded-2xl w-full p-3 text-4xl disabled:bg-gray-700 ";
+            Eggbutton.innerText = "Hatch";
+            if (Ele.heart < 1) {
+                Eggbutton.disabled = true;
+            } 
+            Eggbutton.addEventListener("click", (a) => {
+                HatchDuckEgg(Ele);
+            })
+
+            duckbuttons.appendChild(Eggbutton);
+
+            div.appendChild(duckbuttons);
+
+            duckArea.appendChild(div);
+
+        }
+    
+    });
+
+    localStorage.setItem("DuckEggs", JSON.stringify(DuckEggs))
+
+}
+
+reRenderDuckEggs();
